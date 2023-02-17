7ae35fc6d831a0837109fa8209fa4299e4770bab
Albas-tomi <@albustomi679@gmail.com>
2023-02-17 12:42:09 +0700

    revisi Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ import React from 'react';
 
 function Header({ Background }) {
   return (
-    <nav className={`${Background === 'blue' ? 'bg-blue-500' : 'bg-black/10'} fixed z-20 left-20 md:left-60 border-solid backdrop-blur-md border-b-2 border-gray-100 h-28 w-full text-blue-800 top-0 m-4 text-xl font-bold`}>
-      Home
+    <nav className={`${Background === 'blue' ? 'bg-blue-500' : 'bg-black/10'} fixed z-20 left-20 md:left-60 border-solid backdrop-blur-md border-b-2 border-gray-100 h-28 w-full`}>
+      <h1 className="text-blue-800 top-0 m-4 text-xl font-bold">
+        Home
+      </h1>
     </nav>
   );
 }
