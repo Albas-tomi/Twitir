2ebac9af842b13aa05b2e8084f3517bc44d2639e
Muhammad Albas Tomi <albustomi679@gmail.com>
2023-02-26 19:19:03 +0700

    make error

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -8,7 +8,7 @@ function useInput(defaultValue = '') {
   function handleValueChange({ target }) {
     setValue(target.value);
   }
-  return [value, handleValueChange, setValue];
+  return [value, setValue];
 }
 
 export default useInput;
